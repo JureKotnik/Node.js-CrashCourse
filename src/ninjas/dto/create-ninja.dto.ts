5bf23644197d4cf56236c9212d22931f6f971485
JureKotnik <jure2002k@gmail.com>
2023-04-19 13:22:43 +0200

    Pipes

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,5 +1,9 @@
+import { IsEnum, MinLength } from "class-validator";
+
 export class CreateNinjaDto {
+    @MinLength(3)
     name: string;
 
-    weapon: 'stars' | 'nunchunks'
+    @IsEnum(['stars','nunchucks'], {message: 'Use correct weapon'})
+    weapon: 'stars' | 'nunchucks'
 }
